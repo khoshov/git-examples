759494ede71bcfed6cf759399f264d771a749b21
Stanislav Khoshov <stanislav.khoshov@ubex.com>
2022-10-22 21:15:48 +0300

    add bye message

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+def print_bye(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Bye, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
